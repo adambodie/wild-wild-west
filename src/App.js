8adef5fd15f6a44a8d682d089433699024b97fe8
adambodie <aduo3000@yahoo.com>
2017-09-13 20:27:44 -0700

    created formUrl function

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import './App.css';
 import axios from 'axios';
 import PhotoList from './Components/PhotoList';
 
-const method = 'flickr.photosets.getPhotos';
-const api_key = '0c3f8d32a28de8434240115b85a28499';
-const photoset_id = '72157688485135075';
-const user_id = '8994820%40N07';
-const per_page = '300';
-const url = `https://api.flickr.com/services/rest/?method=${method}&api_key=${api_key}&photoset_id=${photoset_id}&user_id=${user_id}&per_page=${per_page}&format=json&nojsoncallback=1`;
+
+function formUrl(method, api_key, photoset_id, user_id, per_page) {
+	return `https://api.flickr.com/services/rest/
+	?method=${method}
+	&api_key=${api_key}
+	&photoset_id=${photoset_id}
+	&user_id=${user_id}
+	&per_page=${per_page}
+	&format=json&nojsoncallback=1`;
+}
+
+const url = formUrl('flickr.photosets.getPhotos', 
+					'0c3f8d32a28de8434240115b85a28499', 
+					'72157688485135075', 
+					'8994820%40N07', 
+					'300'
+					);
 
 export default class App extends Component {
   constructor() {
